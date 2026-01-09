1314781c0dd03cc47d7c1612229a03ab58fcfb92
Ariel Aguilar <arielaguilar1994@gmail.com>
2026-01-09 16:52:42 +0100

    feat: add markdown

--- src/app/presentation/components/chat-bubbles/chat-message/chat-message.ts
+++ src/app/presentation/components/chat-bubbles/chat-message/chat-message.ts
@@ -1,8 +1,9 @@
 import { Component, input } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-chat-message',
-  imports: [],
+  imports: [MarkdownModule],
   templateUrl: './chat-message.html',
 })
 export class ChatMessage {
